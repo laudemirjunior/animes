05f339301b73b550e2faef109d26b768033ebe62
Laudemir <nlaudemir@gmail.com>
2021-10-07 21:13:30 -0300

    style inicial

--- src/pages/info/index.js
+++ src/pages/info/index.js
@@ -1,13 +1,11 @@
 import { Link, useParams } from "react-router-dom";
 
 const Info = ({ members }) => {
-  const { id } = useParams();
+  const { name } = useParams();
   const member = members.find((item) => item.id === id);
 
   return (
     <div>
-      <h1>Detalhes da Empresa</h1>
-      <p>Nome da empresa: {member && member.name}</p>
       <Link to="/">Voltar</Link>
     </div>
   );
